af5d94bc86a68dee6a3176fab63b8889688052f5
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-12-04 20:00:56 +0800

    added vowel counter

--- Word_Vowel_Consonants.py
+++ Word_Vowel_Consonants.py
@@ -11,8 +11,16 @@ def intro():
     print ('\nHello, welcome to word, vowel, and consonant counter\n')
 intro()
 
-def word():
-    word = input('Please type in the sentence here: ')
-    print(word)
+sentence = input('Please type in the sentence here: ').upper()
+vowel = 'A' 'E' 'I' 'O' 'U'
 
-word()
+def vowel_counter(sentence, vowel):
+    vcount = 0
+    for c in sentence:
+        for d in vowel:
+            if c == d:
+                vcount = vcount + 1
+    return vcount
+
+vowel_count = vowel_counter(sentence, vowel)
+print(vowel_count)
