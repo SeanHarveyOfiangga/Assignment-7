f6c9a22aa89457c99e2bedda65e541e4862ef406
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-12-04 22:50:45 +0800

    Program Completed

--- Password_Validator.py
+++ Password_Validator.py
@@ -10,10 +10,12 @@
 
 #Tip: loop through each character of the input then process it letter by letter
 
+#intro
 def intro():
     print("\n\033[47m\033[30m**Welcome to password validator app!**\033[0m")
 intro()
 
+#password validator function
 def password():
     print("""\n\033[4mPassword Requirements\033[0m
     Your password should have:
@@ -21,31 +23,34 @@ def password():
         \033[3m\033[92mAtleast one uppercase letter\033[0m
         \033[3m\033[35mAtleast one number\033[0m
         \033[3m\033[94mAtleast one special character (!#$%&()*+,-./:;<=>?@[\]^_{|}~)\033[0m
-    ********************************************************************""")
+    ********************************************************************""") #docstring for password requirements
+    #special characters variable
     specialchar = '$' '@' '#' '!' '%' '&' '(' ')' '*' '+' ',' '-' '.' ';' '/' ':' '<' '=' '>' '?' '[' ']' '^' '_' '{' '|''}' '~'
     while True:
         passwordvalidity = True
         try:
-            password = input("\n\033[32mPlease enter a password: \033[0m")
+            password = input("\n\033[32mPlease enter a password: \033[0m")  #input
         except ValueError:
-            print("\033[31mPassword Invalid")
+            print("\033[31mPassword Invalid")       #value error validation
         if len(password) == 0:
-            print("\033[31mPassword is empty\033[0m")
+            print("\033[31mPassword is empty\033[0m")       #checks if the password is empty or not
             passwordvalidity = False
         if len(password) < 15:
-            print("\033[31mPassword is too short\033[0m")
+            print("\033[31mPassword is too short\033[0m")   #checks if the password is short or not
             passwordvalidity = False
         if not any(character.isupper() for character in password):
-            print("\033[31mPassword should have atleast one uppercase letter\033[0m")
+            print("\033[31mPassword should have atleast one uppercase letter\033[0m")       #checks if the password has atleast one uppercase letter or not
             passwordvalidity = False
         if not any(character.isdigit() for character in password):
-            print("\033[31mPassword should have atleast one number\033[0m")
+            print("\033[31mPassword should have atleast one number\033[0m")     #checks if the password has atleast one number
             passwordvalidity = False
         if not any(character in specialchar for character in password):
-            print("\033[31mPassword should have atleast one special character\033[0m")
+            print("\033[31mPassword should have atleast one special character\033[0m")  #checks if the password has atleast one special character
             passwordvalidity = False
-        else:
-            break
+        elif passwordvalidity == True:
+            print("\n\033[42m\033[30mPassword is valid\033[0m")     #if the password is valid this condition will apply
+            print("\nThank you for using the password validator app!")
+            exit()
 
-    
+#call for the function  
 password()
